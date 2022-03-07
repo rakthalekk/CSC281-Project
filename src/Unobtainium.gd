ff836b68d28c1b1d12084d16f28bd1020219772f
Jack Lanois <64977444+jackdeath5@users.noreply.github.com>
2022-03-07 16:28:57 -0500

    Wrong Folder

--- src/Unobtainium.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-# This is the value for how much unobtainium is in a stack
-var count : int = 1
-
-func _on_Unobtainium_body_entered(body):
-	queue_free() #this deletes the entity when it is collected
-	body.unobtainiumCount += count
