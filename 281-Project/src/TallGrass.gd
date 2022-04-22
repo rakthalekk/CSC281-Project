df7b235217486ec4e77e0dc107ebbb8294b861c3
NormallyNormal <33298484+NormallyNormal@users.noreply.github.com>
2022-04-21 20:44:17 -0400

    added diagonals to tall grass

--- 281-Project/src/TallGrass.gd
+++ 281-Project/src/TallGrass.gd
@@ -14,6 +14,6 @@ func generate():
 	for x in range (fieldLength):
 		for y in range (fieldWidth):
 			var rand = noise_map.openSimplex2D(x/8.0, y/8.0)
-			if( rand > 99 ):
+			if( rand > 0.5 ):
 				set_cell(x, y, 0, false, false, false, Vector2(0, 0))
 	update_bitmask_region(Vector2(0, 0), Vector2(fieldLength, fieldWidth))
