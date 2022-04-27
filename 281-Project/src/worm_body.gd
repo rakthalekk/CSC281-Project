0a9fdeba70a2c790df0de0a20f535750a85af3e2
NormallyNormal <33298484+NormallyNormal@users.noreply.github.com>
2022-04-27 12:40:51 -0400

    Added snakes textures and modified body behavior

--- 281-Project/src/worm_body.gd
+++ 281-Project/src/worm_body.gd
@@ -4,8 +4,9 @@ var parent: KinematicBody2D = null
 
 func follow_parent() -> void:
 	self.global_transform.origin = lerp(self.global_transform.origin, 
-		parent.global_transform.origin, get_physics_process_delta_time() * 1.0)
+		parent.global_transform.origin, get_physics_process_delta_time() * 1.5)
 	#self.rotation_degrees = lerp(self.rotation_degrees, 
 	#	parent.rotation_degrees, get_physics_process_delta_time() * 0)
 func _process(delta: float) -> void:
-	follow_parent()
+	if (self.global_transform.origin.distance_squared_to(parent.global_transform.origin) > 5000):
+		follow_parent()
