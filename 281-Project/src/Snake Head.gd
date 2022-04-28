9d2efebc3f4a836bc915f0f0e6878b2fa6e340c3
jrlanois <jrlanois@ncsu.edu>
2022-04-28 11:56:51 -0400

    Cleaned up code a little bit and made atBorder returning active

--- 281-Project/src/Snake Head.gd
+++ 281-Project/src/Snake Head.gd
@@ -4,18 +4,18 @@ extends KinematicBody2D
 onready var tilemap = get_node("../../Navigation2D/TileMap")
 
 #Modifiable Variables
-var absoluteMinY = 32 #The absolute minY where the snake will turn around regardless
-var minY = 40 #The Y where the snake will start to turn around
-var maxY = 64#tilemap.fieldWidth # The Y where the Snake is normal
+var absoluteMinY = 24 #The absolute minY where the snake will turn around regardless
+var minY = 40 #The Y where the snake will start to turn around/max depth of snake wandering
 var minX = 0 #The X where the snake will start to turn around
-var maxX = 64#tilemap.fieldLength #The X where the snake will start to turn around
+var maxY = 64 # The Y where the Snake is normal
+var maxX = 64 #The X where the snake will start to turn around
 var speed = 200 #The Speed at which the snake moves
 
 #Scene Variables
 #var target = null
 var knockback = false
 var targets = []
-#var atBorder = false #a variable to control what the snake does when at the border
+var atBorder = false #a variable to control what the snake does when at the border
 
 # Declare member variables here. Examples:
 var direction := Vector2.ZERO
@@ -30,12 +30,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#Get the Coords of the snake value of the snake
-	var tilePos = Vector2(tilemap.world_to_map(global_position)[0], 64 - 1 - tilemap.world_to_map(global_position)[1])
+	var tilePos = Vector2(tilemap.world_to_map(global_position)[0], maxY - 1 - tilemap.world_to_map(global_position)[1])
 	
 	#Wander toward a target if it has one
 	#The snake will turn back around regardless of what it's attacking
-	if(tilePos[1] < absoluteMinY):# && !atBorder):
-		#atBorder = true
+	if(tilePos[1] < absoluteMinY && !atBorder):
+		atBorder = true
 		if(direction[0] > 0 && direction[1] > 0):
 			direction = direction.rotated(rng.randf_range(-0.2,0)) #Turn Counterclockwise
 		elif(direction[0] < 0 && direction[1] > 0):
@@ -43,7 +43,7 @@ func _process(delta):
 		else:
 			direction = direction.rotated(rng.randf_range(-0.2,0.2))
 	#The snake will attack targets if they exists
-	elif(targets.size() > 0):# && !atBorder):
+	elif(targets.size() > 0 && !atBorder):
 		#The snake will auto target the target at the first index
 		var ang = direction.angle_to(global_position.direction_to(targets[0].global_position))
 		if( ang > 0 && ang <= PI):
@@ -86,7 +86,7 @@ func _process(delta):
 			direction = direction.rotated(rng.randf_range(-0.2,0.2))
 	#The snake will randomly wander when not targeting anything
 	else:
-		#atBorder = false
+		atBorder = false
 		direction = direction.rotated(rng.randf_range(-0.2,0.2)) #Rotates the vector by a given amount
 	direction = direction.normalized()
 	velocity = direction * speed
@@ -95,9 +95,13 @@ func _process(delta):
 
 
 func _on_VisionRadius_body_entered(body):
-	if(Vector2(tilemap.world_to_map(body.global_position)[0], 64 - 1 - tilemap.world_to_map(body.global_position)[1])):
-		pass
-	if body is Player:
+	#if the target is past the absolute minY
+	if(maxY - 1 - tilemap.world_to_map(body.global_position)[1] < minY):
+		#If the target is less than the absolute min, then they will be removed from the targets
+		var idx = targets.find(body)
+		if idx != -1:
+			targets.remove(idx)
+	elif body is Player:
 		#target = body
 		targets.insert(0, body)
 	elif body is Structure:
@@ -108,6 +112,5 @@ func _on_VisionRadius_body_exited(body):
 	var idx = targets.find(body)
 	if idx != -1:
 		targets.remove(idx)
-
 	#if body is Player:
 	#	target = null
